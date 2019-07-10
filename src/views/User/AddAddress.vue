caebc31cb68a09fb798b7b4af1b976457fa89f38
Kirin <825147098@qq.com>
2019-07-10 18:50:48 +0800

    2

--- src/views/User/AddAddress.vue
+++ src/views/User/AddAddress.vue
@@ -5,34 +5,51 @@
         </el-header>
         <el-main>
             <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
-                <el-form-item label="收货人姓名:" prop="name">
-                    <el-input v-model="ruleForm.peopleName"></el-input>
+                <el-form-item  prop="name" class="item_he">
+                    <div class="item_lab">收货人姓名:</div>
+                        <el-input v-model="ruleForm.peopleName"
+                                  style="width: 230px; "
+                        ></el-input>
                 </el-form-item>
-                <el-form-item label="所在地区:">
+                <el-form-item  class="item_he">
+                    <div class="item_lab">所在地区:</div>
                     <el-cascader :props="props"></el-cascader>
                 </el-form-item>
-                <el-form-item label="详细地址:" prop="address">
-                    <el-input v-model="ruleForm.address"></el-input>
+                <el-form-item  prop="address"  class="item_he">
+                    <div class="item_lab">详细地址:</div>
+                    <el-input v-model="ruleForm.address"
+                              style="width: 500px; height: 33px!important;"
+                    ></el-input>
                 </el-form-item>
-                <el-form-item label="手机号码:" prop="phone">
-                    <el-input v-model="ruleForm.phone"></el-input>
+                <el-form-item  prop="phone" class="item_he" style="margin-top: 40px;height: 50px">
+                    <div class="item_lab" style="display: inline;margin-right: 60px">手机号码:</div>
+                    <el-input v-model="ruleForm.phone"
+                    style="width: 230px"
+                    ></el-input>
                 </el-form-item>
-                <el-form-item label="固定电话:" prop="telephone">
-                    <el-input v-model="ruleForm.telephone"></el-input>
+                <el-form-item  prop="telephone" class="item_he" style="height: 50px">
+                    <div class="item_lab" style="display: inline;margin-right: 60px">固定电话:</div>
+                    <el-input v-model="ruleForm.telephone"
+                              style="width: 230px"
+                    ></el-input>
                 </el-form-item>
-                <el-form-item label="邮箱地址:" prop="email">
-                    <el-input v-model="ruleForm.email"></el-input>
+                <el-form-item  prop="email" class="item_he" style="height: 50px">
+                    <div class="item_lab" style="display: inline; margin-right: 60px">邮箱地址:</div>
+                    <el-input v-model="ruleForm.email" style="width: 260px"></el-input>
                 </el-form-item>
-                <el-form-item label="地址别名" prop="tagName">
-                    <el-input v-model="ruleForm.tagName"></el-input>
-                    <div>建议填写常用名称</div>
-                    <el-button plain>家里</el-button>
-                    <el-button plain>父母家</el-button>
-                    <el-button plain>公司</el-button>
-                    <el-button plain>学校</el-button>
+                <el-form-item  prop="tagName" class="item_he">
+                    <div class="item_lab">地址别名:</div>
+                    <el-input v-model="ruleForm.tagName" style="width: 150px"></el-input>
+                    <div
+                            class="item_lab"
+                            style="display: inline">建议填写常用名称</div>
+                    <el-button plain size="small">家里</el-button>
+                    <el-button plain size="small">父母家</el-button>
+                    <el-button plain size="small">公司</el-button>
+                    <el-button plain size="small">学校</el-button>
                 </el-form-item>
-                <el-form-item>
-                    <el-button plain type="primary"> 保存收货地址</el-button>
+                <el-form-item align="center" style="margin-top: 20px">
+                    <el-button plain type="primary" > 保存收货地址</el-button>
                 </el-form-item>
             </el-form>
         </el-main>
@@ -77,5 +94,17 @@
 </script>
 
 <style scoped>
-
+.item_lab{
+    font-size: 12px;
+    color: #999;
+    width: 100px;
+    height:40px;
+    text-align: left;
+    padding: 5px 10px;
+}
+    .item_he{
+        height: 90px;
+        margin: 0;
+        align-items: center;
+    }
 </style>
