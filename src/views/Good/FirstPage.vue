b5ca89bcf876358b76a1e2a9bd4454812073d0e3
Kirin <825147098@qq.com>
2019-07-24 21:27:35 +0800

    Merge branch 'gaoxin' of https://github.com/FHamster/dolldrobe-font into master3
    # Conflicts:
    #	src/views/Good/GoodsPage.vue
    #	src/views/User/BorderModule/UserCenter.vue

---
+++ src/views/Good/FirstPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-container>
+        <el-header>
+
+        </el-header>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "FirstPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
