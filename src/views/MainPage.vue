972ddbbaa8ae35b422cb2ec174f4b0f9c4b7b40e
FHamster <gaoxin459716010@163.com>
2019-07-26 10:24:20 +0800

    商品卡片修改

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -8,27 +8,24 @@
             </el-carousel>
         </el-header>
         <el-main>
-            <el-dialog title="商品详情" :visible.sync="isGoodDialogVis" width="1000px">
-                <GoodDialog :c-num="curGood" :c-name="curGoodName"></GoodDialog>
-            </el-dialog>
             <el-main style="height: 560px ; display: flex; justify-content: center; flex-wrap: wrap" >
                 <el-header height="30px" style="width: 1100px">
                     <div style="font-size: 28px; font-family: 黑体;display: inline;margin-right: 20px">新品首发</div>
                     <div style="font-size: 14px;display: inline">衣橱里总觉得少一件</div>
                     <el-button type="text" style="float: right">更多新品</el-button>
                 </el-header>
-                <el-main style="height: 490px;width: 1200px;display: flex; justify-content: center;" :interval="4000">
-                    <el-carousel arrow="never" height="360px" indicator-position="outside"  style="width: 1140px;justify-content: center">
-                        <el-carousel-item v-for="goods in newList" :key="goods" >
-                            <template v-for="good in goods"  @click="visGoodDialog(good.cNum,good.cName)">
-                                <GoodCard class="card"
-                                          :key="good" :good="good" />
-                            </template>
+                <div style="height: 490px;width: 1200px;display: flex; justify-content: center;" :interval="4000">
+                    <el-carousel arrow="never" height="365px" indicator-position="outside"
+                                 style="width: 1140px;justify-content: center">
+                        <el-carousel-item v-for="goods in newList" :key="goods">
+                            <span v-for="good in goods" :key="good.cNum">
+                                <GoodCard class="card" :good="good"/>
+                            </span>
                         </el-carousel-item>
                     </el-carousel>
-                </el-main>
+                </div>
             </el-main>
-            <el-main style="height: 560px ; display: flex; justify-content: center; flex-wrap: wrap; background-color: #e2cdb1">
+            <el-main class="a-block" style="background-color: #e2cdb1">
                 <el-header height="30px" style="width: 1100px">
                     <div style="font-size: 28px; font-family: 黑体;display: inline;margin-right: 20px">人气画稿</div>
                     <div style="font-size: 14px;display: inline">快去看看，说不定就做出来了呢</div>
@@ -51,7 +48,7 @@
             return {
                 curGood: '',
                 curGoodName: '',
-                isGoodDialogVis: false,
+                isGoodDialogVis2: true,
 
                 item: [{
                     url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564047462046&di=416866d09fa13a973ff69861166420ee&imgtype=0&src=http%3A%2F%2Ftour.btggl.com%2Fupfile%2FC_CABEBCCEBBDHDAJBIHJ%2F2014122618175598.jpg'
@@ -65,48 +62,48 @@
                         cImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564047462046&di=416866d09fa13a973ff69861166420ee&imgtype=0&src=http%3A%2F%2Ftour.btggl.com%2Fupfile%2FC_CABEBCCEBBDHDAJBIHJ%2F2014122618175598.jpg',
                         cName: 'kjfkdsjfsd',
                         cMinmoney: '44.12',
-                        num:0,
+                        num: 0,
                     }, {
                         cImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564047511077&di=58ecae960761e006304e0e8a6dd02668&imgtype=0&src=http%3A%2F%2Fwww.goodmorningtenerife.com%2Fwp-content%2Fuploads%2F2015%2F02%2F01b_slide_excursions_GM_Tenerife-1920x420.jpg',
                         cName: 'kjfkdsjfsfsd',
                         cMinmoney: '44.12',
-                        num:1,
-                    },{
+                        num: 1,
+                    }, {
                         cImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564047462046&di=416866d09fa13a973ff69861166420ee&imgtype=0&src=http%3A%2F%2Ftour.btggl.com%2Fupfile%2FC_CABEBCCEBBDHDAJBIHJ%2F2014122618175598.jpg',
                         cName: 'kjfkdsjfsd',
                         cMinmoney: '44.12',
-                        num:0,
+                        num: 0,
                     }, {
                         cImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564047511077&di=58ecae960761e006304e0e8a6dd02668&imgtype=0&src=http%3A%2F%2Fwww.goodmorningtenerife.com%2Fwp-content%2Fuploads%2F2015%2F02%2F01b_slide_excursions_GM_Tenerife-1920x420.jpg',
                         cName: 'kjfkdsjfsfsd',
                         cMinmoney: '44.12',
-                        num:1,
+                        num: 1,
                     },],
                     [{
                         cImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564047462046&di=416866d09fa13a973ff69861166420ee&imgtype=0&src=http%3A%2F%2Ftour.btggl.com%2Fupfile%2FC_CABEBCCEBBDHDAJBIHJ%2F2014122618175598.jpg',
                         cName: 'kjfkdsjfsd',
                         cMinmoney: '44.12',
-                        num:0,
+                        num: 0,
                     }, {
                         cImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564047511077&di=58ecae960761e006304e0e8a6dd02668&imgtype=0&src=http%3A%2F%2Fwww.goodmorningtenerife.com%2Fwp-content%2Fuploads%2F2015%2F02%2F01b_slide_excursions_GM_Tenerife-1920x420.jpg',
                         cName: 'kjfkdsjfsfsd',
                         cMinmoney: '44.12',
-                        num:1,
-                    },{
+                        num: 1,
+                    }, {
                         cImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564047462046&di=416866d09fa13a973ff69861166420ee&imgtype=0&src=http%3A%2F%2Ftour.btggl.com%2Fupfile%2FC_CABEBCCEBBDHDAJBIHJ%2F2014122618175598.jpg',
                         cName: 'kjfkdsjfsd',
                         cMinmoney: '44.12',
-                        num:0,
+                        num: 0,
                     }, {
                         cImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564047511077&di=58ecae960761e006304e0e8a6dd02668&imgtype=0&src=http%3A%2F%2Fwww.goodmorningtenerife.com%2Fwp-content%2Fuploads%2F2015%2F02%2F01b_slide_excursions_GM_Tenerife-1920x420.jpg',
                         cName: 'kjfkdsjfsfsd',
                         cMinmoney: '44.12',
-                        num:1,
+                        num: 1,
                     },],
                 ],
             }
         },
-        methods:{
+        methods: {
             visGoodDialog(viscNum, visName) {
                 this.curGood = viscNum;
                 this.curGoodName = visName;
@@ -131,11 +128,20 @@
         margin: 0;
         width: 200px;
     }
-    .card{
-        width: 265px;
-        height: 345px;
-        display: block;
+
+    .card {
+        /*width: 265px;*/
+        /*height: 345px;*/
+        display: flex;
         float: left;
         margin-left: 15px
+    }
+
+    .a-block {
+        height: 560px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        flex-wrap: wrap
     }
 </style>
